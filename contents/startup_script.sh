a925ba2d99f0f62217fa2f156d66e329e8fb8134
2swap <mchontz10@gmail.com>
2023-11-25 10:20:11 -0800

    startup script run animator

--- contents/startup_script.sh
+++ contents/startup_script.sh
@@ -1,6 +1,7 @@
 chromium chat.openai.com https://online-go.com/ &
 anki &
 nautilus ~/CS/swaptube &
+gnome-terminal --working-directory="~/CS/swaptube" -- /bin/bash -c './build_and_run.sh Solutions'
 
 udisksctl mount -b /dev/disk/by-uuid/cef5e949-58a7-404d-bb47-695bd3b08d73
 gnome-terminal --working-directory="/media/swap/primary/immersion-tools/watcher/" -- /bin/bash -c 'python3 watcher.py; exec bash'
@@ -8,5 +9,3 @@ nautilus /media/swap/primary/immersion-tools/watcher
 
 #gnome-terminal -- /bin/bash -c 'sudo wireshark &'
 #gnome-terminal -- /bin/bash -c 'watch -n 1 nvidia-smi; exec /bin/bash'
-
-( sleep 60 && xinput map-to-output "HID 256c:006d Pen Pen (0)" "HDMI-0" ) >> ~/autostart.log 2>&1
